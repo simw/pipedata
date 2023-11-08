262aed70a17ae0c51e34d56416bb7d253f38eab0
simw <simw@users.noreply.github.com>
2023-11-08 12:42:32 +0000

    Make parquet file writing start at 1 rather than 0, for ease of reading

--- src/pipedata/ops/storage.py
+++ src/pipedata/ops/storage.py
@@ -5,6 +5,15 @@ import pyarrow.parquet as pq  # type: ignore
 
 from pipedata.core.chain import batched
 
+# Option to accumulate the pyarrow table more frequently
+# so that doesn't need whole list(dict) and pyarrow table
+# in memory at the same time
+
+# Option to hae row_group_length and max_file_length dpendent
+# on size of data, as opposed to number of just numbers of rows.
+# Can combine this with the existing settings, so runs
+# at the smaller of the two.
+
 
 def parquet_writer(
     file_path: str,
@@ -24,7 +33,7 @@ def parquet_writer(
 
     def parquet_writer_func(records: Iterator[Dict[str, Any]]) -> Iterator[str]:
         writer = None
-        file_number = 0
+        file_number = 1
         file_length = 0
         for batch in batched(records, row_group_length):
             table = pa.Table.from_pylist(batch, schema=schema)
