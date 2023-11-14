ac36067d2873250e0793f2daa63344dde25211e9
Simon <simw@users.noreply.github.com>
2023-11-14 08:52:55 +0000

    Add read_from_parquet operation (#10)

--- src/pipedata/ops/files.py
+++ src/pipedata/ops/files.py
@@ -1,11 +1,27 @@
 import logging
 import zipfile
 from dataclasses import dataclass
-from typing import IO, Iterator
+from typing import (
+    IO,
+    Any,
+    Callable,
+    Dict,
+    Iterator,
+    List,
+    Literal,
+    Optional,
+    Union,
+)
 
 import fsspec  # type: ignore
+import pyarrow as pa  # type: ignore
+import pyarrow.dataset as pa_dataset  # type: ignore
 
 logger = logging.getLogger(__name__)
+
+
+class FilesReaderError(Exception):
+    pass
 
 
 @dataclass
@@ -30,3 +46,32 @@ def zipped_files(file_refs: Iterator[str]) -> Iterator[OpenedFileRef]:
                             name=name,
                             contents=inner_file,
                         )
+
+
+def read_from_parquet(
+    columns: Optional[Union[List[str], Dict[str, Any]]] = None,
+    return_as: Literal["recordbatch", "record"] = "record",
+    batch_size: Optional[int] = 100_000,
+) -> Callable[[Iterator[str]], Iterator[Union[Dict[str, Any], pa.RecordBatch]]]:
+    logger.info(f"Initializing parquet reader with {batch_size=}")
+
+    if return_as not in ("recordbatch", "record"):
+        raise FilesReaderError(f"Unknown return_as value {return_as}")
+
+    def parquet_batch_reader(
+        file_refs: Iterator[str],
+    ) -> Iterator[Union[Dict[str, Any], pa.RecordBatch]]:
+        for file_ref in file_refs:
+            logger.info(f"Reading parquet file {file_ref}")
+            ds = pa_dataset.dataset(file_ref, format="parquet")
+            for batch in ds.to_batches(columns=columns, batch_size=batch_size):
+                if return_as == "recordbatch":
+                    yield batch
+                elif return_as == "record":
+                    yield from batch.to_pylist()
+                else:
+                    raise FilesReaderError(
+                        f"Unknown return_as value {return_as}"
+                    )  # pragma: no cover
+
+    return parquet_batch_reader
